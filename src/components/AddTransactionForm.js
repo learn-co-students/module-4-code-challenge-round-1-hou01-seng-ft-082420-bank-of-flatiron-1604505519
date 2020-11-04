f76b6002cbf55975a8af8b079f84e8599a80a179
wellyyy <wesleydfaircloth@gmail.com>
2020-11-04 11:35:01 -0600

    submit

--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,10 +1,39 @@
 import React, { Component } from "react";
 
 class AddTransactionForm extends Component {
+
+  state = {
+    date: "",
+    description: "",
+    category: "",
+    amount: 0
+  }
+
+
+  handleChange = (e) =>{
+    console.log('lets fucking go')
+    e.preventDefault()
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit = (e) =>{
+    e.preventDefault()
+    console.log("form")
+    let newTran = {
+      date: this.state.date,
+      description: this.state.description,
+      category: this.state.category,
+      amount: this.state.amount
+    }
+    console.log(newTran)
+  }
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onChange={this.handleChange} onSubmit= {this.handleSubmit}>
           <div className="inline fields">
             <input type="date" name="date" />
             <input type="text" name="description" placeholder="Description" />
@@ -16,7 +45,7 @@ class AddTransactionForm extends Component {
               step="0.01"
             />
           </div>
-          <button className="ui button" type="submit">
+          <button className="ui button" type="submit" >
             Add Transaction
           </button>
         </form>
--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,38 @@ import React, { Component } from "react";
 import AccountContainer from "./AccountContainer";
 import "../stylesheets/App.css";
 
+const URL = 'http://localhost:6001/transactions'
+
 class App extends Component {
+
+  state = {
+    allTransactions: []
+  }
+
+  componentDidMount(){
+    fetch(URL)
+    .then(res => res.json())
+    .then(transactions => {
+      this.setState({
+        allTransactions: transactions
+      })
+    })
+  }
+
+  addTrans = (newTran) => {
+      this.state({
+        allTransactions: [...this.state.allTransactions, newTran]
+      })
+  }
+ 
+
   render() {
     return (
       <div className="ui raised segment">
         <div className="ui segment violet inverted">
           <h2>The Royal Bank of Flatiron</h2>
         </div>
-        <AccountContainer />
+        <AccountContainer transactions={this.state.allTransactions}/>
       </div>
     );
   }
