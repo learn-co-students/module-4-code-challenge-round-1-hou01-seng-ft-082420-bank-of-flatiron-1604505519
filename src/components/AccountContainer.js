169bed2e22f6f6cce9f5299bc429569980d3387e
jlocke203 <jadenlocke1113@gmail.com>
2020-11-04 11:38:44 -0600

    done

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -2,14 +2,19 @@ import React, { Component } from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
+import Transaction from "./Transaction";
+const transURL = "http://localhost:6001/transactions"
 
 class AccountContainer extends Component {
+
+
   render() {
+    console.log(this.props)
     return (
       <div>
         <Search />
-        <AddTransactionForm />
-        <TransactionsList />
+        <AddTransactionForm addNewTransaction={this.props.addNewTransaction}/>
+        <TransactionsList transactions= {this.props.transactions} />
       </div>
     );
   }
--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,54 @@
 import React, { Component } from "react";
 import AccountContainer from "./AccountContainer";
 import "../stylesheets/App.css";
+const transURL = "http://localhost:6001/transactions"
 
 class App extends Component {
+  state = {
+    allTransactions:[],
+    search:''
+  }
+
+  addNewTransaction = (newTransaction) => {
+    console.log("submit")
+    console.log(newTransaction)
+    fetch(transURL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(newTransaction),
+    })
+    .then(res => res.json())
+    .then((newTransaction) => {
+      this.setState({
+        allTransactions: [...this.state.allTransactions, newTransaction]
+      })
+    })
+  }
+
+
+
+  componentDidMount(){
+    fetch(transURL)
+    .then(res => res.json())
+    .then((transaction) => {
+      this.setState({
+        allTransactions: transaction
+      })
+    })
+  }
+  
+
+
   render() {
     return (
       <div className="ui raised segment">
         <div className="ui segment violet inverted">
           <h2>The Royal Bank of Flatiron</h2>
         </div>
-        <AccountContainer />
+        <AccountContainer transactions={this.state.allTransactions} addNewTransaction={this.addNewTransaction} />
       </div>
     );
   }
