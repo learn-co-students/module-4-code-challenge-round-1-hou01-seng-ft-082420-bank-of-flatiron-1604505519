0844410756cd3937505f78efffa9db9e129a1e3f
denimcouch <alexmata92@ymail.com>
2020-11-04 10:49:37 -0600

    alex mata core deliverables

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -3,13 +3,76 @@ import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
+const baseURL = "http://localhost:6001/transactions/"
+
 class AccountContainer extends Component {
+
+  state = {
+    transactions: [],
+    searchTerm: ''
+  }
+
+  componentDidMount(){
+    fetch(baseURL)
+    .then(res => res.json())
+    .then(transactions => {
+      this.setState({
+        transactions: transactions
+      })
+    })
+  }
+
+  addTransaction = (transaction) => {
+    console.log(transaction)
+    // debugger
+
+    let transactionObj = {
+      date: transaction.date,
+      description: transaction.description,
+      category: transaction.category,
+      amount: parseFloat(transaction.amount)
+    }
+
+    let transactionOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(transactionObj)
+    }
+
+    fetch(baseURL, transactionOptions)
+    .then(res => res.json())
+    .then(newTransaction => {
+      this.setState({
+        transactions: [newTransaction, ...this.state.transactions]
+      })
+    })
+
+  }
+
+  searchTransactions = ({value}) => {
+    this.setState({
+      searchTerm: value
+    })
+  }
+
   render() {
+
+    let filteredTransactions = this.state.transactions.filter( transaction => transaction.description.toLowerCase().includes(this.state.searchTerm.toLowerCase()))
+
     return (
       <div>
-        <Search />
-        <AddTransactionForm />
-        <TransactionsList />
+        <Search 
+        searchTransactions={this.searchTransactions}
+        />
+        <AddTransactionForm 
+        addTransaction={this.addTransaction}
+        />
+        <TransactionsList 
+        transactions={filteredTransactions}
+        />
       </div>
     );
   }
--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,19 +1,40 @@
 import React, { Component } from "react";
 
 class AddTransactionForm extends Component {
+
+  state = {
+    date: '',
+    description: '',
+    category: '',
+    amount: null   
+  }
+
+  handleChange = (e) => {
+    let {name, value} = e.target
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.addTransaction(this.state)
+  }
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={(e) => this.handleSubmit(e) } >
           <div className="inline fields">
-            <input type="date" name="date" />
-            <input type="text" name="description" placeholder="Description" />
-            <input type="text" name="category" placeholder="Category" />
+            <input type="date" name="date" onChange={(e) => this.handleChange(e)} />
+            <input type="text" name="description" placeholder="Description" onChange={(e) => this.handleChange(e)}/>
+            <input type="text" name="category" placeholder="Category" onChange={(e) => this.handleChange(e)} />
             <input
               type="number"
               name="amount"
               placeholder="Amount"
               step="0.01"
+              onChange={(e) => this.handleChange(e)}
             />
           </div>
           <button className="ui button" type="submit">
