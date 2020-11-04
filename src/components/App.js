d746dbedc45ed1a9a476b6651ec4520e0efea310
Paytongray18 <paytongray18@gmail.com>
2020-11-04 11:42:11 -0600

    done

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,29 @@ import React, { Component } from "react";
 import AccountContainer from "./AccountContainer";
 import "../stylesheets/App.css";
 
+
+
+
 class App extends Component {
+ 
+
+    state = {
+      allTransactions: []
+    }
+  
+  componentDidMount = () => {
+  fetch('http://localhost:6001/transactions')
+  .then(res => res.json())
+  .then(data => this.setState({allTransactions: data}))
+  }
+
   render() {
     return (
       <div className="ui raised segment">
         <div className="ui segment violet inverted">
           <h2>The Royal Bank of Flatiron</h2>
         </div>
-        <AccountContainer />
+        <AccountContainer transactionData ={this.state.allTransactions} />
       </div>
     );
   }
