fdff5acd500825372a0c08d3c74f5623ad538913
Billyhelm <billyhelm88@gmail.com>
2020-11-04 10:30:02 -0700

    Billys Code Challenge

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -4,12 +4,53 @@ import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 class AccountContainer extends Component {
+  state = {
+    allTransactions: [],
+    searchTerm: '',
+    filteredTrans: []
+  }
+
+  // waits for the page to load and then goes and fetches all the 
+  // transactions in API, sets this info as state
+  componentDidMount(){
+    fetch("http://localhost:6001/transactions").then(res=>res.json())
+    .then(data => this.setState({allTransactions: data}))
+    
+  } 
+
+  handleSearch = (e) => {
+    let value = e.target.value
+    console.log(e.target.value)
+    console.log(value)
+    this.setState({
+      searchTerm: value,
+      filteredTrans: this.state.allTransactions.filter(trans => trans.description.toLowerCase().includes(this.state.searchTerm.toLowerCase()))})
+  }
+
+
+  newTransaction = (newTrans) => {
+    //console.log("new transaction submitted", newTrans)
+    fetch("http://localhost:6001/transactions", {
+      method: "POST",
+      headers: {
+        "Content-Type":"application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(newTrans)
+    }).then(res=>res.json())
+    .then(trans => this.setState({allTransactions: [...this.state.allTransactions, trans]}))
+  }
+
+
+
   render() {
     return (
       <div>
-        <Search />
-        <AddTransactionForm />
-        <TransactionsList />
+        <Search handleSearch={this.handleSearch}/>
+        <AddTransactionForm newTransaction={this.newTransaction}/>
+        {/* passing transactions object into the transactionsList */}
+        <TransactionsList transactions={this.state.searchTerm ? this.state.filteredTrans : this.state.allTransactions}/>
+
       </div>
     );
   }
--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,22 +1,45 @@
 import React, { Component } from "react";
 
+
 class AddTransactionForm extends Component {
+
+  state = {
+    date: "",
+    description: "",
+    category: "",
+    amount: ""
+  }
+  
+  handleChange = (e) => {
+    let {name, value} = e.target
+    this.setState({[name]: value})
+    //console.log("I changed", this.state[name])
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    let form = document.querySelector("#newForm")
+    form.reset()
+    this.props.newTransaction(this.state)
+  }
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" id="newForm" onSubmit={(e)=>this.handleSubmit(e)}>
           <div className="inline fields">
-            <input type="date" name="date" />
-            <input type="text" name="description" placeholder="Description" />
-            <input type="text" name="category" placeholder="Category" />
+            <input type="date" name="date" onChange={(e)=>this.handleChange(e)}/>
+            <input type="text" name="description" placeholder="Description" onChange={(e)=>this.handleChange(e)}/>
+            <input type="text" name="category" placeholder="Category" onChange={(e)=>this.handleChange(e)}/>
             <input
               type="number"
               name="amount"
               placeholder="Amount"
               step="0.01"
+              onChange={(e)=>this.handleChange(e)}
             />
           </div>
-          <button className="ui button" type="submit">
+          <button className="ui button" type="submit" >
             Add Transaction
           </button>
         </form>
