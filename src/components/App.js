0c03cf9f9f6c85f065a201c5e1d87f73479ab9b3
mderosa777 <meggsnbacon777@gmail.com>
2020-11-04 11:36:13 -0600

    meggs

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,36 @@
 import React, { Component } from "react";
 import AccountContainer from "./AccountContainer";
 import "../stylesheets/App.css";
-
+//import Transactions  from "./Transaction.js"
 class App extends Component {
-  render() {
+
+  state={
+      transactions: []
+    }
+  
+
+
+   componentDidMount(){
+  fetch("http://localhost:6001")
+  .then (res => res.json())
+  .then (transactions =>{
+    console.log(transactions)
+    //this.setState({
+      //transactions: transactions
+    })
+    
+ // })
+}
+
+  
+  
+    render() {
     return (
       <div className="ui raised segment">
         <div className="ui segment violet inverted">
           <h2>The Royal Bank of Flatiron</h2>
         </div>
-        <AccountContainer />
+        <AccountContainer  transactions = {this.state.transactions}/>
       </div>
     );
   }
