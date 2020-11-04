ddaf430e36e3e7c02b45a70462de207fc52f7ab3
BenLooper <benlooper08@gmail.com>
2020-11-04 10:39:58 -0600

    submission for deadline

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -4,12 +4,55 @@ import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 class AccountContainer extends Component {
+
+  constructor(){
+    super();
+    this.state = ({
+      transactions: [],
+      filteredTransactions: []
+    })
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:6001/transactions')
+    .then(res=>res.json())
+    .then(transactions => this.setState({
+      transactions: transactions,
+      filteredTransactions: transactions
+    }))
+  }
+
+  addNewTransaction = (transaction) => {
+    fetch('http://localhost:6001/transactions',{
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(transaction)
+    })
+    .then(res=>res.json())
+    .then(newTransaction=>this.setState({
+      transactions: [...this.state.transactions,newTransaction]
+    }))
+  }
+
+  searchTransaction = (e) => {
+    let searchTerm = e.target.value.toLowerCase();
+    console.log(searchTerm)
+    this.setState({
+      filteredTransactions: this.state.transactions.filter(
+        transaction=>(transaction.description.toLowerCase()).includes(searchTerm)
+      )
+    })
+  }
+
   render() {
     return (
       <div>
-        <Search />
-        <AddTransactionForm />
-        <TransactionsList />
+        <Search searchTransaction={this.searchTransaction}/>
+        <AddTransactionForm addNewTransaction={this.addNewTransaction}/>
+        <TransactionsList transactions={this.state.filteredTransactions}/>
       </div>
     );
   }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-const Search = () => {
+const Search = ({searchTransaction}) => {
   return (
     <div className="ui large fluid icon input">
       <input
         type="text"
         placeholder={"Search your Recent Transactions"}
-        onChange={() => {
-          console.log("Searching...");
+        onChange={(e) => {
+          searchTransaction(e)
         }}
       />
       <i className="circular search link icon"></i>
