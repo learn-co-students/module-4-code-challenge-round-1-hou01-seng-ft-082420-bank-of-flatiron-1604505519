f50d17392e584b2d125d3e5a5feface9b44d75c2
bebomwonyi <bebomwonyi@gmail.com>
2020-11-04 11:35:42 -0600

    an attempt was made

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,32 @@ import AccountContainer from "./AccountContainer";
 import "../stylesheets/App.css";
 
 class App extends Component {
+state={
+  trans: []
+}
+  componentDidMount(){
+    fetch('http://localhost:6001/transactions')
+    .then(res=>res.json())
+    .then(tran=>{
+      this.setState({
+        trans: tran
+      })
+    })
+  }
+
   render() {
     return (
       <div className="ui raised segment">
         <div className="ui segment violet inverted">
           <h2>The Royal Bank of Flatiron</h2>
         </div>
-        <AccountContainer />
+        <AccountContainer boop={this.state.trans}/>
       </div>
     );
   }
 }
 
 export default App;
+
+
+// dont worry i know
